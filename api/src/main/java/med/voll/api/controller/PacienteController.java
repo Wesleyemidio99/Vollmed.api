eb3adce7ed0383d6de7cd32895ddaed8935ea2e9
João Guilherme <jaaumgui@gmail.com>
2024-02-26 18:14:28 -0300

    refactor(PacienteController): Utilizando anotação Slf4j no lugar do print default

--- api/src/main/java/med/voll/api/controller/PacienteController.java
+++ api/src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,17 +1,19 @@
 package med.voll.api.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import med.voll.api.paciente.DadosCadastroPaciente;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("paciente")
 public class PacienteController {
 
     @PostMapping
     public void cadastrar(@RequestBody DadosCadastroPaciente dados){
-        System.out.println("Dados recebidos do paciente: " +dados);
+        log.info("Dados recebidos do paciente: {}", dados);
     }
 }
