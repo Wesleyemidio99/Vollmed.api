3a223b3bae7cdf6061cbcbaa4f2c9d4425e933e0
João Guilherme <jaaumgui@gmail.com>
2024-02-26 18:08:39 -0300

    refactor(Medico): Utilizando equals e hashcode adaptado para hibernate

--- api/src/main/java/med/voll/api/medico/Medico.java
+++ api/src/main/java/med/voll/api/medico/Medico.java
@@ -5,35 +5,50 @@ import lombok.*;
 import med.voll.api.endereco.Endereco;
 import med.voll.api.medico.dto.DadosCadastroMedico;
 import med.voll.api.medico.dto.Especialidade;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.util.Objects;
 
 
-@Table(name = "medicos")
-@Entity(name = "Medico")
+@Entity
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode(of = "id")
+@Table(name = "medicos")
 public class Medico {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nome;
+
     private String email;
+
     private String telefone;
+
     private String crm;
 
     @Enumerated(EnumType.STRING)
     private Especialidade especialidade;
+
     @Embedded
     private Endereco endereco;
 
-    public Medico(DadosCadastroMedico dados) {
-        this.nome = dados.nome();
-        this.email = dados.email();
-        this.crm = dados.crm();
-        this.telefone = dados.telefone();
-        this.especialidade = dados.especialidade();
-        this.endereco = new Endereco(dados.endereco());
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Medico medico = (Medico) o;
+        return getId() != null && Objects.equals(getId(), medico.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
     }
 }
 
