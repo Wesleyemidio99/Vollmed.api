72455bbd82ee422db192ded619deba1f507fef8d
João Guilherme <jaaumgui@gmail.com>
2024-02-26 18:07:17 -0300

    refactor(DadosCadastroMedico): mudado de record para classe (melhor integração com o mapstruct)

--- api/src/main/java/med/voll/api/medico/dto/DadosCadastroMedico.java
+++ api/src/main/java/med/voll/api/medico/dto/DadosCadastroMedico.java
@@ -5,26 +5,37 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
+import lombok.*;
 import med.voll.api.endereco.dto.DadosEndereco;
 
-public record DadosCadastroMedico(
-                                  @NotBlank
-                                  String nome,
-                                  @NotBlank
-                                  @Email
-                                  String email,
-                                  @NotBlank
-                                  String telefone,
+import java.util.Objects;
 
-                                  @NotBlank
-                                  @Pattern(regexp = "\\d{4,6}")
-                                  String crm,
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@RequiredArgsConstructor
+public class DadosCadastroMedico {
 
-                                  @NotNull
-                                  Especialidade especialidade,
+    @NotBlank
+    private final String nome;
 
-                                  @NotNull
-                                  @Valid
-                                  DadosEndereco endereco) {
+    @Email
+    @NotBlank
+    private final String email;
+
+    @NotBlank
+    private final String telefone;
+
+    @NotBlank
+    @Pattern(regexp = "\\d{4,6}")
+    private final String crm;
+
+    @NotNull
+    private final Especialidade especialidade;
+
+    @Valid
+    @NotNull
+    private final DadosEndereco endereco;
 
 }
