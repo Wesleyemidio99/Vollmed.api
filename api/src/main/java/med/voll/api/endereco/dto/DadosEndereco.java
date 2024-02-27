e19edc64b781fa191611759b770c70110fea135f
wesley <wesleyemidio99@hotmail.com>
2024-02-27 16:09:54 -0300

    Alterado de dados endereço de record para class

--- api/src/main/java/med/voll/api/endereco/dto/DadosEndereco.java
+++ api/src/main/java/med/voll/api/endereco/dto/DadosEndereco.java
@@ -2,18 +2,30 @@ package med.voll.api.endereco.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
-public record DadosEndereco(@NotBlank
-                            String logradouro,
-                            @NotBlank
-                            String bairro,
-                            @NotBlank
-                            @Pattern(regexp = "\\d{8}")
-                            String cep,
-                            @NotBlank
-                            String cidade,
-                            @NotBlank
-                            String uf,
-                            String numero,
-                            String complemento) {
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class DadosEndereco {
+
+    @NotBlank
+    private final String logradouro;
+
+    @NotBlank
+    private final String bairro;
+
+    @NotBlank
+    @Pattern(regexp = "\\d{8}")
+    private final String cep;
+
+    @NotBlank
+    private final  String cidade;
+
+    @NotBlank
+    private final String uf;
+    private final String numero;
+    private final String complemento;
 }
