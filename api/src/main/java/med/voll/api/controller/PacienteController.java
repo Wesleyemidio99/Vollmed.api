056f4a2cb4caaa321845e1535c02bb7187dbe95a
wesley <wesleyemidio99@hotmail.com>
2024-02-27 16:29:23 -0300

    Instanciei service dentro de controller, e fiz o cadastro de paciente

--- api/src/main/java/med/voll/api/controller/PacienteController.java
+++ api/src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,19 +1,24 @@
 package med.voll.api.controller;
 
-import lombok.extern.slf4j.Slf4j;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import med.voll.api.paciente.dto.PacienteDto;
+import med.voll.api.repository.PacienteRepository;
+import med.voll.api.services.PacienteService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
+
 @RestController
 @RequestMapping("paciente")
+@RequiredArgsConstructor
 public class PacienteController {
 
+    private final PacienteService service;
     @PostMapping
-    public void cadastrar(@RequestBody PacienteDto dados){
-        log.info("Dados recebidos do paciente: {}", dados);
+    public void cadastrar(@RequestBody @Valid PacienteDto dados){
+        service.cadastrar(dados);
     }
 }
