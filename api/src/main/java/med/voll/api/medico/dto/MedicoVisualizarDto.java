352ca8277211c02c3f39617cb2a001579d2216bc
wesley <wesleyemidio99@hotmail.com>
2024-03-23 22:48:04 -0300

    Retirando comentario

--- api/src/main/java/med/voll/api/medico/dto/MedicoVisualizarDto.java
+++ api/src/main/java/med/voll/api/medico/dto/MedicoVisualizarDto.java
@@ -1,13 +1,9 @@
 package med.voll.api.medico.dto;
 
 import lombok.Value;
-import med.voll.api.medico.Medico;
 
 import java.io.Serializable;
 
-/**
- * DTO for {@link Medico}
- */
 @Value
 public class MedicoVisualizarDto implements Serializable {
     Long id;
