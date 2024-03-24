64c2f3c2bf1768bc03b17803d6deedd0fa6bb3b7
wesley <wesleyemidio99@hotmail.com>
2024-03-23 22:49:01 -0300

    Implementando o metodo de listar paciente

--- api/src/main/java/med/voll/api/services/PacienteService.java
+++ api/src/main/java/med/voll/api/services/PacienteService.java
@@ -2,11 +2,16 @@ package med.voll.api.services;
 
 import lombok.RequiredArgsConstructor;
 import med.voll.api.mappings.PacienteMapper;
+import med.voll.api.paciente.Paciente;
 import med.voll.api.paciente.dto.PacienteDto;
+import med.voll.api.paciente.dto.PacienteVisualizarDto;
 import med.voll.api.repository.PacienteRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Validated
@@ -19,5 +24,15 @@ public class PacienteService {
     @Transactional(rollbackFor = Exception.class)
     public void cadastrar(PacienteDto cadastroPaciente){
         repository.save(mapper.toEntity(cadastroPaciente));
+    }
+
+    public List<PacienteVisualizarDto> listarPaciente(){
+        List<Paciente> pacienteList = repository.findAll();
+        List<PacienteVisualizarDto> pacienteVisualizarDtoList = new ArrayList<>();
+        for (Paciente paciente:
+             pacienteList) {
+            pacienteVisualizarDtoList.add(mapper.toDto1(paciente));
+        }
+        return pacienteVisualizarDtoList;
     }
 }
